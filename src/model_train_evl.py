01b08019c2d0457fd46001f4e6762eabc7e395ea
sudu1499 <suduanagha@gmail.com>
2021-09-30 23:41:02 +0530

    added lots of things

---
+++ src/model_train_evl.py
@@ -0,0 +1,67 @@
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import ColumnTransformer
+from get_param import get_param
+from argparse import ArgumentParser
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import accuracy_score
+import pickle as pk
+import os
+import json
+def metrics(ypred,ytest):
+    score=accuracy_score(ytest,ypred)
+    print('accuracy score for knn model ',score)
+    return score
+def model(config_path):
+    config=get_param(config_path)
+    train_=config['split']['train']
+    test_=config['split']['test']
+    n=config['model']['knn']['params']['n_neighbours']
+    train=pd.read_csv(train_)
+    test=pd.read_csv(test_)
+    xtrain=train.iloc[:,[0,1,4,6,10,11,12]].values
+    ytrain=train['salary'].values
+    xtest=test.iloc[:,[0,1,4,6,10,11,12]].values    
+    ytest=test['salary'].values
+
+    le=LabelEncoder()
+    ytrain=le.fit_transform(ytrain)
+
+    le=LabelEncoder()
+    ytest=le.fit_transform(ytest)
+    
+    ct=ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[1,3])],remainder='passthrough')
+    ct.fit(xtrain)
+    xtrain=ct.transform(xtrain).toarray()
+
+    xtest=ct.fit_transform(xtest).toarray()
+
+    knn=KNeighborsClassifier(n_neighbors=n)
+    knn.fit(xtrain,ytrain)
+
+    ypred=knn.predict(xtest)
+
+    score=metrics(ypred,ytest)
+    model_=config['saved_model']
+
+    pk.dump(knn,open(os.path.join(model_,'knnmodel.pkl'),'wb'))
+    pk.dump(ct,open(os.path.join(model_,'ct.pkl'),'wb'))
+
+    param_=config['report']['params']
+    metrics_=config['report']['metrics']
+
+    with open(param_,'w') as f:
+        t=json.dumps(config['model']['knn']['params'])
+        f.write(t)
+
+    with open(metrics_,'w') as f:
+        t=json.dumps({'accuracy':score})
+        f.write(t)
+
+if __name__=='__main__':
+    arg=ArgumentParser()
+    arg.add_argument('--config',default='params.yaml')
+    parsed=arg.parse_args()
+    model(parsed.config)
+    
---
+++ web_app/app.py
@@ -0,0 +1,38 @@
+from flask import Flask,request,render_template,url_for,redirect
+import pickle
+import numpy as np
+import logging
+app=Flask(__name__,static_folder='static',template_folder='templates')
+logging.basicConfig(filename='sudu.txt',filemode='a',format='%(asctime)s %(levelname)s-%(message)s',
+                    datefmt='%Y-%m-%d %H-%M-%S')
+try:
+    model=pickle.load(open('knnmodel.pkl','rb'))
+except:
+    logging.critical("no model pickel file")
+try:
+    ct=pickle.load(open('ct.pkl','rb'))
+except:
+    logging.critical('no scaler pickel file')
+
+@app.route('/')
+def index():
+    return render_template('indata.html')
+@app.route('/predicted',methods=['GET','POST'])
+def i2():
+    pred1=[x for x in request.form.values()]
+    pred1[0]=float(pred1[0])
+    pred1[2]=float(pred1[2])
+    pred1[4]=float(pred1[4])
+    pred1[5]=float(pred1[5])    
+    pred1[6]=float(pred1[6])
+    x=np.reshape(pred1,(1,-1))
+    pred1=ct.transform(x).toarray()
+    pred1=model.predict(pred1)
+    if pred1[0]==0:
+        msg='expected income is below 50 k'
+    else:
+        msg='expected income is above 50 k'
+    return render_template('predicted.html',pred=msg) 
+
+if __name__=='__main__':
+    app.run(debug=True,port=8000)
