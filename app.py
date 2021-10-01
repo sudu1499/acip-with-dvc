84f507bb2db6c399f026915c57351647d88db173
sudu1499 <suduanagha@gmail.com>
2021-10-01 18:50:34 +0530

    PROC_file updated

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ webapp_root = "web_app"
 static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
 
-app=Flask(__name__,static_folder=static_dir,template_folder=template_dir)
+app=Flask(__name__,static_folder='static',template_folder='templates')
 logging.basicConfig(filename='sudu.txt',filemode='a',format='%(asctime)s %(levelname)s-%(message)s',
                     datefmt='%Y-%m-%d %H-%M-%S')
 try:
@@ -43,4 +43,4 @@ def i2():
         return render_template('predicted.html',pred=msg) 
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True) 
+    app.run( port=5000, debug=True) 
