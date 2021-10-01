0dc8d2a826c3da2882263bc1c61d085f6a94c102
sudu1499 <suduanagha@gmail.com>
2021-10-01 10:57:02 +0530

    PROC_file updated

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ def i2():
         msg='expected income is below 50 k'
     else:
         msg='expected income is above 50 k'
+    print('*********************************************************************************************************',msg)
     return render_template('predicted.html',pred=msg) 
 
 if __name__=='__main__':
