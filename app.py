5fa4edefd8204fbba6708a6e9a151e2e36e34b6a
sudu1499 <suduanagha@gmail.com>
2021-10-01 12:55:12 +0530

    PROC_file updated

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def i2():
     return render_template('templates/predicted.html',pred=msg) 
 
 if __name__=='__main__':
-    app.run(debug=True,port=8000) 
+    app.run(host='0.0.0.0', port=5000, debug=True) 
